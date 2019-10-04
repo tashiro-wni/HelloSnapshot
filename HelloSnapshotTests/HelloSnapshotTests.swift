e6810227b1af8cacfcebea9ffd3eef15b33ddeb4
Tomohiro Tashiro <tashiro@wni.com>
2019-10-04 18:15:31 +0900

    Initial Commit

---
+++ HelloSnapshotTests/HelloSnapshotTests.swift
@@ -0,0 +1,34 @@
+//
+//  HelloSnapshotTests.swift
+//  HelloSnapshotTests
+//
+//  Created by Tashiro Tomohiro on 2019/10/04.
+//  Copyright © 2019 weathernews. All rights reserved.
+//
+
+import XCTest
+@testable import HelloSnapshot
+
+class HelloSnapshotTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
